54cd4ec3a0c94f7c06f3cad34f44f11a9a4c87b4
typable <contact@typable.dev>
2021-12-15 11:10:24 +0100

    Upgraded config to ron file

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly HOST="typable@git.typable.dev"
+readonly HOST="typable@192.168.2.3"
 readonly TARGET="bin/dns-rs"
 readonly ARCH="armv7-unknown-linux-gnueabihf"
 readonly SOURCE="./target/$ARCH/release/dns-rs"
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,109 @@
 use std::fs;
-use std::collections::HashMap;
-use props_rs::{parse, to_map};
+use ron::de::from_str;
+use serde::Deserialize;
 use serde_json::Value;
 use regex::Regex;
 use reqwest::StatusCode;
 
-fn main() {
-    let props = env("config.properties");
-    let ip = fetch_ip(&props);
-    let resolve = fetch_resolve(&props);
-    println!("local: {}", ip);
-    println!("domain: {}", resolve);
-    if ip.eq(&resolve) {
-        println!("status: UP_TO_DATE");
-    }
-    else {
-        println!("status: OUTDATED");
-        let success = fetch_update(&props, &ip);
-        println!("updated: {}", success);
+#[derive(Debug, Deserialize)]
+struct Provider {
+    url: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Domain {
+    domain: String,
+    host: String,
+}
+
+impl ToString for Domain {
+    fn to_string(&self) -> String {
+        if self.host.is_empty() || self.host.eq("@") {
+            format!("{}", self.domain)
+        }
+        else {
+            format!("{}.{}", self.host, self.domain)
+        }
     }
 }
 
-fn fetch_ip(props: &HashMap<String, String>) -> String {
-    let provider_ip = props.get("dns.provider.ip").take()
-        .expect("Unable to retrieve property!");
-    let resp = reqwest::blocking::get(provider_ip)
+#[derive(Debug, Deserialize)]
+struct Config {
+    provider: Provider,
+    domains: Vec<Domain>,
+}
+
+const IP_RESOLVER_URL: &str = "http://checkip.amazonaws.com";
+const DOMAIN_RESOLVER_URL: &str = "https://dns.google.com/resolve";
+
+fn main() {
+    let config = read_config("config.ron")
+        .expect("Unable to read config file!");
+    let ip = resolve_ip();
+    for domain in config.domains {
+        let resolved_domain = resolve_domain(&domain);
+        if !ip.eq(&resolved_domain) {
+            if request_update(&config.provider, &domain, &ip) {
+                println!("Updated {} to {}", domain.to_string(), ip);
+            }
+            else {
+                println!("Failed to update {} to {}", domain.to_string(), ip);
+            }
+        }
+    }
+}
+
+fn resolve_ip() -> String {
+    let resp = reqwest::blocking::get(IP_RESOLVER_URL)
         .expect("Unable to resolve ip provider!");
     let body = resp.text()
         .expect("Unable to retrieve response text");
     body.trim().to_string()
 }
 
-fn fetch_resolve(props: &HashMap<String, String>) -> String {
-    let provider_resolve = props.get("dns.provider.resolve").take()
-        .expect("Unable to retrieve property!");
-    let host = props.get("dns.host").take()
-        .expect("Unable to retrieve property!");
-    let domain = props.get("dns.domain").take()
-        .expect("Unable to retrieve property!");
-    let query = format!("{}?name={}.{}&type=A", provider_resolve, host, domain);
+fn resolve_domain(domain: &Domain) -> String {
+    let query = format!(
+        "{}?name={}&type=A",
+        DOMAIN_RESOLVER_URL,
+        domain.to_string(),
+    );
     let resp = reqwest::blocking::get(query)
         .expect("Unable to resolve ip provider!");
     let body = resp.text()
         .expect("Unable to retrieve response json");
     let json: Value = serde_json::from_str(&body)
         .expect("Unable to parse into json!");
-    String::from(json["Answer"][0]["data"].as_str().take().expect("Unable to parse into string!"))
+    String::from(json["Answer"][0]["data"]
+        .as_str().take()
+        .expect("Unable to parse into string!"))
 }
 
-fn fetch_update(props: &HashMap<String, String>, ip: &String) -> bool {
-    let provider_domain = props.get("dns.provider.domain").take()
-        .expect("Unable to retrieve property!");
-    let domain = props.get("dns.domain").take()
-        .expect("Unable to retrieve property!");
-    let host = props.get("dns.host").take()
-        .expect("Unable to retrieve property!");
-    let password = props.get("dns.provider.domain.password").take()
-        .expect("Unable to retrieve property!");
-    let query = format!("{}/update?domain={}&host={}&ip={}&password={}", provider_domain, domain, host, ip, password);
+fn request_update(provider: &Provider, domain: &Domain, ip: &String) -> bool {
+    let query = format!(
+        "{}/update?domain={}&host={}&ip={}&password={}",
+        provider.url,
+        domain.domain,
+        domain.host,
+        ip,
+        provider.password,
+    );
     let resp = reqwest::blocking::get(query)
         .expect("Unable to resolve ip provider!");
     if let StatusCode::OK = resp.status() {
         let body = resp.text()
             .expect("Unable to retrieve response text");
-        let exp = Regex::new(r"<ErrCount>(\d+)</ErrCount>").expect("Unable to parse regex!");
-        let caps = exp.captures(&body).expect("Unable to capture regex!");
-        let status = caps.get(1).map_or("", |group| group.as_str());
+        let exp = Regex::new(r"<ErrCount>(\d+)</ErrCount>")
+            .expect("Unable to parse regex!");
+        let caps = exp.captures(&body)
+            .expect("Unable to capture regex!");
+        let status = caps.get(1)
+            .map_or("", |group| group.as_str());
         return status.eq("0")
     }
     false
 }
 
-fn env(file: &str) -> HashMap<String, String> {
-    let content = fs::read_to_string(file)
-        .expect("Unable to read file!");
-    let parsed = parse(content.as_bytes())
-        .expect("Unable to parse file!");
-    to_map(parsed)
+fn read_config(path: &str) -> Result<Config, ron::Error> {
+    from_str(&fs::read_to_string(path)?)
 }
